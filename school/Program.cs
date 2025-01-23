2504ba8a19037bab02604d1b49719e10b399bdca
zahrahavaei <139990240+zahrahavaei@users.noreply.github.com>
2025-01-23 11:31:13 -0500

    first

---
+++ school/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+using System.Globalization;
+using school;
+
+class program
+{
+    static void Main()
+    {
+       
+        List<Student> studentslist = new List<Student>();
+        Student student = new Student("","",0,"");
+        while (true)
+        {
+            Console.WriteLine("What do you want to do?! ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1: Add a new student ");
+            Console.WriteLine("2: Remove a student ");
+            Console.WriteLine("3:Search a student ");
+            Console.WriteLine("4: update a student ");
+            Console.WriteLine("5: show list of student ");
+            Console.WriteLine("6: Exit ");
+            Console.ResetColor();
+            if (!int.TryParse(Console.ReadLine(), out int num) || (num < 1 || num > 6))
+            {
+                Console.WriteLine("invalid number,try again");
+                continue;
+            }
+
+            switch (num)
+            {
+                case 1:
+                    student.Addstudent(studentslist);
+                    break;
+
+                case 2:
+                    student.Removestudent(studentslist);
+                    break;
+                case 3:
+                    student.Searchstudent(studentslist);
+                    break;
+                case 4:
+                    student.Updatestudent(studentslist);
+                    break;
+                case 5:
+                    student.Showstudentlist(studentslist);
+                    break;
+                case 6:
+                    Console.WriteLine("Exiting...");
+                    return;
+
+            }
+            
+            
+        }
+    }
+   
+    
+}
+
+
+
+
+
+
+
+
+
+
---
+++ school/Student.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school
+{
+    public class Student
+    {
+        private string firstname;
+        private string lastname;
+       
+        private int grade;
+        private string id;
+       
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+       
+        public int Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+        public Student(string firstname, string lastname, int grade, string id)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Grade = grade;
+            Id = id;
+        }
+        public void DisplayStudentInfo()
+        {
+            Console.WriteLine($"Name:{Firstname} {Lastname} Grade:{Grade} Id:{Id}");
+
+        }
+        public void Addstudent(List<Student> studentslist)
+        {
+            Console.WriteLine("enter st name");
+            string stfirstname = Console.ReadLine();
+            Console.WriteLine("enter last name");
+            string stlastname = Console.ReadLine();
+            Console.WriteLine("enter Grade");
+            if (!int.TryParse(Console.ReadLine(), out int stgrade))
+            {
+                Console.WriteLine("invalid grade ");
+                return;
+            }
+            Console.WriteLine("enter Id");
+           string stid= Console.ReadLine();
+            var newstudent = new Student(stfirstname, stlastname, stgrade, stid);
+            studentslist.Add(newstudent);
+
+        }
+        //..............................................................................................
+        public void Removestudent(List<Student> studentslist)
+        {
+            Console.WriteLine("Enter the first name of the student to remove:");
+            string stfirstname = Console.ReadLine();
+            Console.WriteLine("Enter the Id of the student to remove:");
+            string stid = Console.ReadLine();
+            var removestudent = studentslist.Find(st => st.Firstname==(stfirstname)&& st.Id==(stid));
+            if (removestudent != null)
+            {
+                        studentslist.Remove(removestudent);
+                        Console.WriteLine($"Student {stfirstname}  id:{stid} removed successfully.");
+                    
+            }
+            else
+            {
+                Console.WriteLine($"Student {firstname} not found.");
+            }
+        }
+        //.........................................................................
+        public void Updatestudent(List<Student> studentslist)
+        {
+            Console.WriteLine("Enter the first name of the student to remove:");
+            string stfirstname = Console.ReadLine();
+            Console.WriteLine("Enter the Id of the student to remove:");
+            string stid = Console.ReadLine();
+            var studentupdate = studentslist.Find(st => st.Firstname==(stfirstname) && st.Id==(stid));
+            if (studentupdate != null)
+            {
+                Console.WriteLine("enter new name");
+                string newfirstname = Console.ReadLine();
+                studentupdate.Firstname = newfirstname;
+                Console.WriteLine("update was successful");
+            }
+        }
+        //........................................................................
+        public void Searchstudent(List<Student> studentslist)
+        {
+            Console.WriteLine("Enter the first name of the student to search:");
+            string stfirstname = Console.ReadLine();
+
+            var studentfind = studentslist.Where(s => s.Firstname==(stfirstname));
+            if (studentfind != null )
+            {
+                foreach(var st in studentfind)
+                {
+                    Console.WriteLine("Student found:");
+                    st.DisplayStudentInfo();
+                }
+                
+               
+            }
+            else
+            {
+                Console.WriteLine($"Student {stfirstname} not found.");
+            }
+        }
+        //.........................................................
+        public void Showstudentlist(List<Student> studentsList)
+        {
+            if (studentsList.Count == 0)
+            {
+                Console.WriteLine("No students to display.");
+                return;
+            }
+
+            Console.WriteLine("List of Students:");
+            foreach (Student s in studentsList)
+            {
+                s.DisplayStudentInfo();
+            }
+
+
+        }
+    }
+}
